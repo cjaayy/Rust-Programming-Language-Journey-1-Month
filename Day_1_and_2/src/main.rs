c1e4758ae9b65430d3e90694238fcdb7778ec490
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2025-04-18 17:10:02 +0800

    Day 1 and 2

---
+++ Day_1_and_2/src/main.rs
@@ -0,0 +1,29 @@
+/*
+- install tools that enhance Rust development in IDEs
+In the terminal, type: 
+rustup component add rls rust-analysis rust-src
+
+- Create a new Rust project
+In the terminal, type:
+cargo new my_project_name --bin
+
+- Navigate into the correct project folder
+In the terminal, type:
+cd "hello_rust"
+
+- Run the Program
+In the VSCode terminal, type:
+cargo run
+- You should see this output:
+   Compiling hello_rust v0.1.0
+    Finished dev [unoptimized + debuginfo] target(s) in ...
+     Running `target/debug/hello_rust`
+Hello, World!
+
+*/
+
+// First Hello World in Rust
+fn main() {
+    println!("Hello, World!");
+    println!("Hello, I'm CJay!");
+}
