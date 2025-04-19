b809c9175a9fcd8133761f00291a5471fb59f39b
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2025-04-19 20:03:02 +0800

    Day 4

---
+++ Day_4/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Integer variables
+    let age: i32 = 30;
+    let height: u64 = 180;
+
+    // Float variable
+    let weight: f64 = 75.5;
+
+    // Boolean variable
+    let is_rust_fun: bool = true;
+
+    // Char variable
+    let first_initial: char = 'J';
+
+    // Printing variables
+    println!("Age: {}", age);
+    println!("Height: {} cm", height);
+    println!("Weight: {} kg", weight);
+    println!("Is Rust fun?: {}", is_rust_fun);
+    println!("First initial: {}", first_initial);
+}
