ddfb05b86652534698209df8a16ee1cfd5f0ef9c
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2025-04-21 21:40:58 +0800

    Day 6

---
+++ Day_6/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    // This is a single-line comment
+
+    /*
+        This is a multi-line comment.
+        You can write detailed explanations here.
+    */
+
+    let a = 10;
+    let b = 3;
+
+    // Addition
+    let sum = a + b;
+    println!("{} + {} = {}", a, b, sum);
+
+    // Subtraction
+    let difference = a - b;
+    println!("{} - {} = {}", a, b, difference);
+
+    // Multiplication
+    let product = a * b;
+    println!("{} * {} = {}", a, b, product);
+
+    // Division
+    let quotient = a / b;
+    println!("{} / {} = {}", a, b, quotient);
+
+    // Remainder (modulus)
+    let remainder = a % b;
+    println!("{} % {} = {}", a, b, remainder);
+}
